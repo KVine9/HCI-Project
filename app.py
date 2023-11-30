ec61e4a3c676b2497700834be2c7aa6bdfb83883
KVine9 <152453378+KVine9@users.noreply.github.com>
2023-11-30 14:15:53 -0500

    Update app.py
    Alejandro adds sidebar

--- app.py
+++ app.py
@@ -23,26 +23,56 @@ def display_results(results, result_type):
         st.warning(f"No {result_type}s found.")
 
 def search_artist(artist_name):
+    st.sidebar.subheader("Search Options")
+    selected_tab = st.sidebar.radio("Select Tab", ["Raw MusicBrainz API data", "Filtered Data", "Artist Information"])
 
-    search_url = f"{base_url}artist/"
-    params = {'query': artist_name, 'fmt': 'json'}
-    results = make_request(search_url, params)
-    display_results(results, 'artists')
+    if selected_tab == "Raw MusicBrainz API data":
+        search_url = f"{base_url}artist/"
+        params = {'query': artist_name, 'fmt': 'json'}
+        results = make_request(search_url, params)
+        display_results(results, 'artists')
+    elif selected_tab == "Filtered Data":
+        # Add logic for filtered data
+        pass
+    elif selected_tab == "Artist Information":
+        # Add logic for artist information
+        pass
 
 def search_album(album_name):
-    search_url = f"{base_url}release/"
-    params = {'query': album_name, 'fmt': 'json'}
-    results = make_request(search_url, params)
-    display_results(results, 'releases')
+    st.sidebar.subheader("Search options")
+    selected_tab = st.sidebar.radio("Select Tab", ["Raw MusicBrainz API Data", "Filtered Data", "Release Events"])
+
+    if selected_tab == "Raw MusicBrainz API Data":
+        search_url = f"{base_url}release/"
+        params = {'query': album_name, 'fmt': 'json'}
+        results = make_request(search_url, params)
+        display_results(results, 'releases')
+    elif selected_tab == "Filtered Data":
+        # Add logic for filtered Data
+        pass
+    elif selected_tab == "Release Events":
+        # Add logic for filtered data
+        pass
 
 def search_song(song_name):
-    search_url = f"{base_url}recording/"
-    params = {'query': song_name, 'fmt': 'json'}
-    results = make_request(search_url, params)
-    display_results(results, 'recordings')
+    st.sidebar.subheader("Search options")
+    selected_tab = st.sidebar.radio("Select Tab", ["Raw MusicBrainz API Data", "Filtered Data", "Length of songs"])
+
+    if selected_tab == "Raw MusicBrainz API Data":
+        search_url = f"{base_url}recording/"
+        params = {'query': song_name, 'fmt': 'json'}
+        results = make_request(search_url, params)
+        display_results(results, 'recordings')
+    elif selected_tab == "Filtered Data":
+        # Add logic for filtered Data
+        pass
+    elif selected_tab == "Length of songs":
+        # Add logic for filtered data
+        pass
 
 def main():
     st.title("Music Search App")
+    st.markdown("<title style = 'text-align': center; 'color': blue; '>Music Search App </title>", unsafe_allow_html=True)
     search_query = st.text_input("Enter a song, album, or artist name")
     search_type = st.selectbox("Search Type", ["Song", "Album", "Artist"])
 
