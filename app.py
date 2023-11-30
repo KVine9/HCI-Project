3c4e184167875e091a984ae30ced960486b4c344
KVine9 <152453378+KVine9@users.noreply.github.com>
2023-11-30 14:13:23 -0500

    Update app.py
    Alejandro's update. Makes code more readable and display the data in tables

--- app.py
+++ app.py
@@ -1,87 +1,45 @@
 import requests
 import streamlit as st
 
+
 base_url = "https://musicbrainz.org/ws/2/"
-def search_artist(artist_name):
-    search_url = f"{base_url}artist/"
 
-    # Set up parameters for the request
-    params = {
-        'query': artist_name,
-        'fmt': 'json'  # Requesting the response in JSON format
-    }
-
-    # Make the request
+def make_request(search_url, params):
     response = requests.get(search_url, params=params)
-
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-
-        # Extract relevant information
-        if 'artists' in data:
-            artists = data['artists']
-            for artist in artists:
-                st.write(f"Name: {artist['name']}, ID: {artist['id']}")
-        else:
-            st.write("No artists found.")
+        return response.json()
     else:
-        st.write(f"Error: {response.status_code}")
+        st.error(f"Error: {response.status_code}")
+        return None
+
+def display_results(results, result_type):
+    if results and result_type in results:
+        data = results[result_type]
+        if data:
+            st.table(data)
+        else:
+            st.warning(f"No {result_type}s found.")
+    else:
+        st.warning(f"No {result_type}s found.")
+
+def search_artist(artist_name):
+
+    search_url = f"{base_url}artist/"
+    params = {'query': artist_name, 'fmt': 'json'}
+    results = make_request(search_url, params)
+    display_results(results, 'artists')
 
 def search_album(album_name):
     search_url = f"{base_url}release/"
-
-    # Set up parameters for the request
-    params = {
-        'query': album_name,
-        'fmt': 'json'  # Requesting the response in JSON format
-    }
-
-    # Make the request
-    response = requests.get(search_url, params=params)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-
-        # Extract relevant information
-        if 'releases' in data:
-            albums = data['releases']
-            for album in albums:
-                st.write(f"Name: {album['title']}, Artist: {album['artist-credit']}, ID: {album['id']}")
-        else:
-            st.write("No artists found.")
-    else:
-        st.write(f"Error: {response.status_code}")
+    params = {'query': album_name, 'fmt': 'json'}
+    results = make_request(search_url, params)
+    display_results(results, 'releases')
 
 def search_song(song_name):
     search_url = f"{base_url}recording/"
-
-    # Set up parameters for the request
-    params = {
-        'query': song_name,
-        'fmt': 'json'  # Requesting the response in JSON format
-    }
-
-    # Make the request
-    response = requests.get(search_url, params=params)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-
-        # Extract relevant information
-        if 'recordings' in data:
-            songs = data['recordings']
-            for song in songs:
-                st.write(f"Name: {song['title']}, Artist: {song['artist-credit']}, ID: {song['id']}")
-        else:
-            st.write("No artists found.")
-    else:
-        st.write(f"Error: {response.status_code}")
+    params = {'query': song_name, 'fmt': 'json'}
+    results = make_request(search_url, params)
+    display_results(results, 'recordings')
 
 def main():
     st.title("Music Search App")
@@ -97,10 +55,3 @@ def main():
             search_song(search_query)
 
 main()
-
-# Example usage
-#search_artist("Metallica")
-#search_album("Graduation")
-#search_song2("No Role Modelz")
-
-#https://coverartarchive.org/release/ID This is a different API tho
